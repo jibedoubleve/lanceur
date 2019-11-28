10eafa49d7ea887ea01409c429c905a9ad213d96
jibedoubleve <jibedoubleve@users.noreply.github.com>
2019-11-28 16:25:40 +0100

    Fix #20 - Fix version number

--- build/update-versions.ps1
+++ build/update-versions.ps1
@@ -7,7 +7,10 @@ $vInno = ".\build\setup.iss"
 
 $version = $version -replace "V", ""
 
-Set-Content -Path $vFile -Value $($(Get-content $vFile) -replace '"([0-9]{1,3}\.?){3}"', """$version""")
+Write-Host "Replace version '$version' in file $vFile"
+Set-Content -Path $vFile -Value $($(Get-content $vFile) -replace '"([0-9]{1,3}\.?){3,4}"', """$version""")
+
+Write-Host "Replace version '$version' in file $vInno"
 Set-Content -Path $vInno -Value $($(Get-Content $vInno) -replace "#define MyAppVersion ""([0-9]{1,3}\.?){3}""", "#define MyAppVersion ""$version""")
 
 Start-Sleep 1
