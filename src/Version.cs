155208a8f9d5ba5994e9ee34116bcd65a0c32a09
jibedoubleve <jibedoubleve@users.noreply.github.com>
2020-04-16 15:09:02 +0200

    (maint) fix release

--- src/Version.cs
+++ src/Version.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.6.1.3")]
-[assembly: AssemblyFileVersion("0.6.1.0")]
-[assembly: AssemblyInformationalVersion("0.6.1-beta.3+Branch.develop.Sha.db99105ea7e403d6ffc699e7005ac34778ddc123")]
+[assembly: AssemblyVersion("0.6.0.53")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
+[assembly: AssemblyInformationalVersion("0.6.0-beta.53+Branch.develop.Sha.19144eeb0827a2eca78015916920d212e0206f9e")]
 
