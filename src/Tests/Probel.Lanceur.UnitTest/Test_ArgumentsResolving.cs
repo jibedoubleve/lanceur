e954a8ef61fb33c1a84146e70c4647e8c248aae1
jibedoubleve <jibedoubleve@users.noreply.github.com>
2020-04-21 16:10:57 +0200

    (maint) Fix unit tests

--- src/Tests/Probel.Lanceur.UnitTest/Test_ArgumentsResolving.cs
+++ src/Tests/Probel.Lanceur.UnitTest/Test_ArgumentsResolving.cs
@@ -1,6 +1,7 @@
 ﻿using NSubstitute;
 using Probel.Lanceur.Core.Services;
 using Probel.Lanceur.Core.ServicesImpl;
+using System.Net;
 using Xunit;
 
 namespace Probel.Lanceur.UnitTest
@@ -35,7 +36,7 @@ namespace Probel.Lanceur.UnitTest
         public void Can_urlify_with_not_supported_char()
         {
             var before = "© #&";
-            var after = "%C2%A9+%23&";
+            var after = WebUtility.UrlEncode(before);
 
             var cb = Substitute.For<IClipboardService>();
             cb.GetText().Returns(string.Empty);
