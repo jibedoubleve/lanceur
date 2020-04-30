c75df5508cae5cbe3ee3b5b0ab4b14d72e16a80e
jibedoubleve <jibedoubleve@users.noreply.github.com>
2020-04-30 14:43:38 +0200

    (maint) Show dependencies

--- src/Version.cs
+++ src/Version.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.7.0.29")]
+[assembly: AssemblyVersion("0.7.0.31")]
 [assembly: AssemblyFileVersion("0.7.0.0")]
-[assembly: AssemblyInformationalVersion("0.7.0-beta.29+Branch.develop.Sha.c8861901dabb525297a030e518a9b9d411535eca")]
+[assembly: AssemblyInformationalVersion("0.7.0-beta.31+Branch.develop.Sha.38aa982a498a90e6c3b9b09fe0bbd3b7396061eb")]
 
