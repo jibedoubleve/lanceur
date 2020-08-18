ef1c83516a3b0be17846ff75d7e49232bc89ebb7
jibedoubleve <jibedoubleve@gmail.com>
2020-08-18 16:39:30 +0200

    Add transaction to see whether the issue still occurs (#137)

--- src/Version.cs
+++ src/Version.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.8.0.14")]
+[assembly: AssemblyVersion("0.8.0.1")]
 [assembly: AssemblyFileVersion("0.8.0.0")]
-[assembly: AssemblyInformationalVersion("0.8.0-alpha.14+Branch.feature-alias-repositories.Sha.c2417265b0e80cbdedffc5158aa835a842b5f59f")]
+[assembly: AssemblyInformationalVersion("0.8.0-alpha.1+Branch.feature-0.8.0-phase-2.Sha.945753e24ca2c9d04a5d8bac1e17feeb3fae22d9")]
 
