01038e284a8ad5b6b541593b49982046589391f9
jibedoubleve <jibedoubleve@gmail.com>
2021-07-07 08:56:49 +0200

    Fix #151 - Crash on alias creation

--- src/Version.cs
+++ src/Version.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.8.0.1")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
-[assembly: AssemblyInformationalVersion("0.8.0-bugfix-issue-137.1+2.Branch.bugfix-issue-137.Sha.c1aae7a5dadc9c6b49ef892fb2aa08596b11b233")]
+[assembly: AssemblyVersion("0.8.1.2")]
+[assembly: AssemblyFileVersion("0.8.1.0")]
+[assembly: AssemblyInformationalVersion("0.8.1-alpha.0-8-1.2+Branch.feature-0.8.1.Sha.b9e49260dff64490ca1f3b24faee2f413213279c")]
 
